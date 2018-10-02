5c19f86936aff522928a7b008c2fe456d9ff73e3
Rawk-Star <jeremydalby@gmail.com>
2018-10-02 09:50:43 -0400

    Part way through project.

---
+++ app/src/main/java/com/rawks/jeremy/alohamusic/MainActivity.java
@@ -0,0 +1,15 @@
+package com.rawks.jeremy.alohamusic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
